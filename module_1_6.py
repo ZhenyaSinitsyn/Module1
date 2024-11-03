09f2908b871eec005f475bded0d7d55383f5e06c
ZhenyaSinitsyn <sinitsyn09@gmail.com>
2024-11-03 19:06:01 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,13 +1,15 @@
 my_dict = {'water': 34, 'juece': 13, 'tea': 56, 'coffe': 17}
-my_dict['water'] = 34
-my_dict['pulpi'] = 11
+print(my_dict)
+print(my_dict['water'])
+print(my_dict.get('pulpi'), 'no such key')
 my_dict.update({'energy': 52,
                 'beer': 24})
 del my_dict['tea']
 print(my_dict)
 
-my_set = [1, 2, 3, 4, 1, 4, 3, 2, (1, 2, 3), 'sport']
-print(set(my_set))
-my_set = {1, 2, 3, 4, 1, 4, 3, 2, (1, 2, 3), True, 'meat', 'sport'}
+my_set = {1, 2, 3, 4, 1, 4, 3, 2, (1, 2, 3), 'sport'}
+print(my_set)
+my_set.add('meat')
+my_set.add('rock')
 print(my_set.discard(1))
 print(my_set)
